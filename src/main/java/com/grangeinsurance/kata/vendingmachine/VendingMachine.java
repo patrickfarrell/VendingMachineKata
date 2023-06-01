f3c3797254315e40ab1cd8557086cc8cd56eef08
farrelp <farrelp@grangeinsurance.com>
2023-06-01 15:42:00 -0400

    JIRA-1234: displayReadsInsertCoinWhenOnePennyInserted - Refactored

--- src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
+++ src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
@@ -1,13 +1,25 @@
 package com.grangeinsurance.kata.vendingmachine;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class VendingMachine {
 
 	private static final String DEFAULT_TEXT = "INSERT COIN";
 	
 	private float totalValue = 0;
 	
+	private Set<Float> validCoins = new HashSet<Float>();
+	
+	public VendingMachine() {
+		validCoins.add(0.25f);
+		validCoins.add(0.10f);
+		validCoins.add(0.05f);
+	}
+	
+	
 	public String getDisplayText() {
-		if (totalValue > .01) {
+		if (totalValue > 0) {
 			return String.format("$%.2f", totalValue);
 		}
 		else {
@@ -16,7 +28,9 @@ public class VendingMachine {
 	}
 
 	public void insertCoin(float value) {
-		totalValue += value;
+		if (validCoins.contains(value)) {
+			totalValue += value;
+		}
 	}
 	
 }
