69f62038a9a188fc18bfb9a7987811e542cf4727
farrelp <farrelp@grangeinsurance.com>
2023-06-01 17:34:06 -0400

    JIRA-1234: coinReturnContainsOnePennyWhenOnePennyInserted - Refactored

--- src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
+++ src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
@@ -1,6 +1,8 @@
 package com.grangeinsurance.kata.vendingmachine;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class VendingMachine {
@@ -8,8 +10,8 @@ public class VendingMachine {
 	private static final String DEFAULT_TEXT = "INSERT COIN";
 	
 	private float totalValue;
-	
 	private Set<Float> validCoins = new HashSet<Float>();
+	private List<Float> coinReturn = new ArrayList<Float>();
 	
 	public VendingMachine() {
 		validCoins.add(0.25f);
@@ -26,11 +28,13 @@ public class VendingMachine {
 	
 	public void insertCoin(float value) {
 		if (validCoins.contains(value)) {
-			totalValue += value;	
+			totalValue += value;
+		} else {
+			coinReturn.add(value);
 		}
 	}
 	
 	public Float[] getCoinReturn() {
-		return new Float[] { 0.01f };
+		return coinReturn.toArray(new Float[0]);
 	}
 }
