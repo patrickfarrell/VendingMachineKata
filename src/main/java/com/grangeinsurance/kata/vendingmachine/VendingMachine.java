7ef053c196ecbc4f40845dc7c4d6f3cf73b9ecb7
farrelp <farrelp@grangeinsurance.com>
2023-06-01 14:27:33 -0400

    Initial commit

---
+++ src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
@@ -0,0 +1,5 @@
+package com.grangeinsurance.kata.vendingmachine;
+
+public class VendingMachine {
+	
+}
---
+++ src/test/java/com/grangeinsurance/kata/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,23 @@
+package com.grangeinsurance.kata.vendingmachine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class ensures that the Vending Machine implementation 
+ * is operating according to the given specifications
+ * 
+ * Approach:
+ * 1. Write a failing test
+ * 2. Make the test pass
+ * 3. Commit
+ * 4. Refactor
+ * 5. Commit
+ * 
+ * @author Patrick Farrell
+ */
+ class VendingMachineTest {
+
+}
