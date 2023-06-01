7423b806c5621e49fb433472f5ec4253264920f0
farrelp <farrelp@grangeinsurance.com>
2023-06-01 15:57:10 -0400

    JIRA-1234: Reset to initial state

--- src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
+++ src/main/java/com/grangeinsurance/kata/vendingmachine/VendingMachine.java
@@ -1,45 +1,5 @@
 package com.grangeinsurance.kata.vendingmachine;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class VendingMachine {
 
-	private static final String DEFAULT_TEXT = "INSERT COIN";
-	
-	private float totalValue = 0;
-	
-	private Set<Float> validCoins = new HashSet<Float>();
-	private List<Float> coinReturn = new ArrayList<Float>();
-	
-	public VendingMachine() {
-		validCoins.add(0.25f);
-		validCoins.add(0.10f);
-		validCoins.add(0.05f);
-	}
-	
-	public String getDisplayText() {
-		if (totalValue > 0) {
-			return String.format("$%.2f", totalValue);
-		}
-		else {
-			return DEFAULT_TEXT;
-		}
-	}
-
-	public void insertCoin(float value) {
-		if (validCoins.contains(value)) {
-			totalValue += value;
-		}
-		else {
-			coinReturn.add(value);
-		}
-	}
-
-
-	public Float[] getCoinReturn() {
-		return coinReturn.toArray(new Float[0]);
-	}
 }
